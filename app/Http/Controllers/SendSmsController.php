60ff88af8a8d041c82546b1f0957d11aeb0a9529
kameshwarchurendra <kameshwarchurendra1995@gmail.com>
2024-01-28 18:15:21 +0530

    initial commit

--- app/Http/Controllers/SendSmsController.php
+++ app/Http/Controllers/SendSmsController.php
@@ -18,24 +18,24 @@ public function index(Request $request){
 public function MessageSend(Request $request){
 
 
-      // Validate the incoming request data
-        // $validator = Validator::make($request->all(), [
-        //     'mobile' => 'required|string|regex:/^\d{10}$/',
-        //     'meassage' => 'required',
+      //Validate the incoming request data
+        $validator = Validator::make($request->all(), [
+            'mobile' => 'required|string|regex:/^\d{10}$/',
+            'meassage' => 'required',
            
-        // ]);
+        ]);
 
-        // // If validation fails, return validation errors to the user
-        // if ($validator->fails()) {
-        //     return redirect()->back()->withErrors($validator)->withInput();
-        // }
+        // If validation fails, return validation errors to the user
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
 
-    $sid    = env('TWILIO_SID');
-    $token  = env('TWILIO_TOKEN'); 
-    $number  = env('TWILIO_NUMBER'); 
+    $sid    = env('TWILIO_SID'); // Change Env File twilio sid  from your twilio account
+    $token  = env('TWILIO_TOKEN');  // Change Env File twilio token  from your twilio account
+    $number  = env('TWILIO_NUMBER');  // Change Env File twilio number  from your twilio account
     $twilio = new Client($sid, $token);
-    $mobile = "+91".$request->mobile; //"+917869677560";//
-    $messages = $request->message; //"Hello I Am Kameshwar"; 
+    $mobile = "+91".$request->mobile; 
+    $messages = $request->message; 
 
     //print_r($sid);die();
     $message = $twilio->messages->create($mobile, 
